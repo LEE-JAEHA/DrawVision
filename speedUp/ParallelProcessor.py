6be58018fffbea4b2b8cf2aaa20bcb4a5bfcb92f
LEE-JAEHA <years6666@gmail.com>
2024-03-16 20:29:30 +0900

    JAEHA : Fix Multiprocess

--- speedUp/ParallelProcessor.py
+++ speedUp/ParallelProcessor.py
@@ -1,4 +1,5 @@
 import multiprocessing
+from functools import partial
 
 class ParallelProcessor:
     def __init__(self, num_processes=None):
@@ -8,24 +9,22 @@ class ParallelProcessor:
             self.num_processes = num_processes
     
     def process_task(self, task_function, data_list):
-        chunk_size = len(data_list) // self.num_processes
-        chunks = [data_list[i:i+chunk_size] for i in range(0, len(data_list), chunk_size)]
-        
         with multiprocessing.Pool(processes=self.num_processes) as pool:
-            results = pool.map(task_function, chunks)
+            results = pool.starmap(task_function, data_list)
         
         return results
 
 # 예시 작업 함수
-def square_numbers(numbers):
-    return [x * x for x in numbers]
+def print_add_three_numbers(text, a, b, c):
+    print(text)
+    return a, b, c
 
 # 사용 예시
 if __name__ == "__main__":
-    data = list(range(1, 11))  # 작업할 데이터 리스트
+    data = [("First set:", 1, 2, 3), ("Second set:", 4, 5, 6), ("Third set:", 7, 8, 9)]  # 작업할 데이터 리스트
     processor = ParallelProcessor(num_processes=4)  # 병렬 처리 객체 생성
     
-    # 데이터를 병렬로 제곱하기
-    squared_results = processor.process_task(square_numbers, data)
+    # 데이터를 병렬로 출력하고 반환하기
+    results = processor.process_task(partial(print_add_three_numbers), data)
     
-    print("Squared Results:", squared_results)
+    print("Results:", results)
