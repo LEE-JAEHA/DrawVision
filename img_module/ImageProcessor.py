c4aada804615b2fc19ba878709bea0cc5c4ce312
LEE-JAEHA <years6666@gmail.com>
2024-03-17 12:35:59 +0900

    JAEHA : Add draw convexhull

--- img_module/ImageProcessor.py
+++ img_module/ImageProcessor.py
@@ -1,5 +1,5 @@
 import cv2
-
+import numpy as np
 class ImageProcessor:
     def __init__(self, image_path):
         self.image = cv2.imread(image_path)
@@ -8,11 +8,22 @@ class ImageProcessor:
     
     def add_bounding_box(self, x_min, y_min, x_max, y_max, color=(0, 255, 0), thickness=2):
         cv2.rectangle(self.image, (x_min, y_min), (x_max, y_max), color, thickness)
-    
+        
     def add_bounding_boxes(self, boxes, color=(0, 255, 0), thickness=2):
         for box in boxes:
             x_min, y_min, x_max, y_max = box
             self.add_bounding_box(x_min, y_min, x_max, y_max, color, thickness)
+    
+    def draw_convex_hull(self, points, color=(0, 255, 0), thickness=2):
+        hull = cv2.convexHull(np.array(points).astype(np.int32))  # Convex Hull 계산
+        cv2.polylines(self.image, [hull], isClosed=True, color=color, thickness=thickness)  # Convex Hull 그리기
+
+    def draw_point(self, point, color=(0, 0, 255), radius=5):
+        cv2.circle(self.image, (int(point[0]),int(point[1])), radius, color, -1)  # 점 그리기
+
+    def draw_points(self, points, color=(0, 0, 255), radius=5):
+        for point in points:
+            self.draw_point(tuple(point), color, radius)  # 모든 점 그리기
     
     def add_text(self, text, x, y, font=cv2.FONT_HERSHEY_SIMPLEX, font_scale=1, color=(255, 255, 255), thickness=2):
         cv2.putText(self.image, text, (x, y), font, font_scale, color, thickness)
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from img_module.ImageProcessor import ImageProcessor
-
+import numpy as np
 if __name__ == "__main__":
     image_processor = ImageProcessor("/Users/jaeha/Desktop/jaeha/son2.png")
     bounding_boxes = [(100, 100, 300, 300), (200, 200, 400, 400)]  # 여러 개의 bounding box 좌표 리스트
-    image_processor.add_bounding_boxes(bounding_boxes)
-    image_processor.add_text("Example Text", 50, 50)
+    
+    np.random.seed(0)
+    points = np.random.rand(30, 2) * 100  # 30개의 랜덤한 (x, y) 좌표 생성 (이 예시에서는 400x400 이미지 내에서 생성)
+    print(points)
+    # image_processor.draw_points(points)
+    image_processor.draw_convex_hull(points)
+    
+    # image_processor.add_bounding_boxes(bounding_boxes)
+    # image_processor.add_text("Example Text", 50, 50)
     image_processor.save_image("./output_image.jpg")
